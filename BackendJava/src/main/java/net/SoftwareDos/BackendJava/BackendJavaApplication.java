e593ed41a4bfa88cf6fbdd0f020710ab76680cd4
unknown <juan.tellez@correo.tdea.edu.co>
2021-09-13 20:40:28 -0500

    Creacion base del proyecto en el repositorio

---
+++ BackendJava/src/main/java/net/SoftwareDos/BackendJava/BackendJavaApplication.java
@@ -0,0 +1,17 @@
+package net.SoftwareDos.BackendJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.SoftwareDos.BackendJava.Controllers.UserController;
+
+@SpringBootApplication
+public class BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaApplication.class, args);
+		new UserController();
+		System.out.println("Holis");
+	}
+
+}
