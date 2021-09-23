8b08ace47ce37cec02c63e589f33f190755d5b62
Juan José Téllez Rojas <88468784+juanjot08@users.noreply.github.com>
2021-09-22 21:45:34 -0500

    Jtellez (#2)
    * Define UserrCreate and begin to define UserServices with an interface
    
    * UserService(Class, Interface), CrudInterface
    
    We finish to define the UserService class and the UserService Interface. Also we make an Entity for User and a crudRepository interface

--- BackendJava/src/main/java/net/SoftwareDos/BackendJava/Controllers/UserController.java
+++ BackendJava/src/main/java/net/SoftwareDos/BackendJava/Controllers/UserController.java
@@ -1,31 +1,45 @@
 package net.SoftwareDos.BackendJava.Controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import net.SoftwareDos.BackendJava.Models.Request.UserDetailRequestModel;
+import net.SoftwareDos.BackendJava.Models.Responses.UserRest;
+import net.SoftwareDos.BackendJava.Services.UserServiceInterface;
+import net.SoftwareDos.BackendJava.Shared.dto.Userdto;
 
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
+    @Autowired
+    UserServiceInterface userService;
+
+
     @GetMapping
     public String getUser() {
         return "Obteniendo usuario";
     }
 
-    @DeleteMapping
-    public String deleteUser() {
-        return "Borrando usuario";
-    }
-
     @PostMapping
-    public String createUser(@RequestBody UserDetailRequestModel userDetails) {
-        return "Creando usuario";
+    public UserRest createUser(@RequestBody UserDetailRequestModel userDetails) {
+
+        UserRest userToReturn = new UserRest();
+
+        Userdto userDTO = new Userdto();
+        
+        BeanUtils.copyProperties(userDetails, userDTO);
+
+        Userdto createdUser = userService.createUser(userDTO);
+
+        BeanUtils.copyProperties(createdUser, userToReturn);
+
+        return userToReturn;
     }
 
 }
