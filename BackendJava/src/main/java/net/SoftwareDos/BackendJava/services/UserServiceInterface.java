82cd6a4bcd4d2256370f0feada2a24025054cb78
Andres De la Hoz <54108311+eldelahoz@users.noreply.github.com>
2021-09-29 21:29:55 -0500

    Delet directory
    Because it ready exist

--- BackendJava/src/main/java/net/SoftwareDos/BackendJava/services/UserServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package net.SoftwareDos.BackendJava.services;
-
-import net.SoftwareDos.BackendJava.shared.dto.UserDTO;
-
-public interface UserServiceInterface {
-    public UserDTO createUser(UserDTO userDto);
-}
