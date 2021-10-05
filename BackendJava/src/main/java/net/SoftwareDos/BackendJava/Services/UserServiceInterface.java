0f80d02bd7de23ae760a4a977be996c716622788
juanjot08 <88468784+juanjot08@users.noreply.github.com>
2021-10-04 20:31:36 -0500

    Commit For Sync

--- BackendJava/src/main/java/net/SoftwareDos/BackendJava/Services/UserServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package net.SoftwareDos.BackendJava.Services;
-
-import net.SoftwareDos.BackendJava.Shared.dto.Userdto;
-
-public interface UserServiceInterface {
-    
-    public Userdto createUser(Userdto userdto);
-
-}
